c9630b609ebf4e11faf92a59ef4baf4c25522ee7
Caleb Manor <CMANOR@harding.edu>
2021-02-24 12:41:11 -0600

    First commit

---
+++ comp3100p4.py
@@ -0,0 +1,13 @@
+# Caleb Manor
+
+# PROGRAM FOUR - comp3100p4.py
+# Create a list that contains peach, banana, pear, apple
+# Add the value grape to the list
+# Sort the list
+# Print the list
+
+fruit_list = ["peach", "banana", "pear", "apple"]
+grape = ["grape"]
+fruit_list = fruit_list + grape
+fruit_list.sort()
+print(fruit_list)
